67bb19ee4cb531a7a0225f6931c30c5e0c60153f
mlaj24 <36448042+mlaj24@users.noreply.github.com>
2025-09-12 14:59:17 +0200

    push name printer

---
+++ adrien_ml/name_printer.py
@@ -0,0 +1,12 @@
+"""Modules for printing names."""
+
+def print_name(name: str) -> None:
+    """
+        Prints the given name.
+        Args:
+            name (str): The name to print.
+    """
+    print(f"Name: {name}")
+
+if __name__ == "__main__":
+    print_name("Adrien")
