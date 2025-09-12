df91d8d76aa8e05c81ddc41022ae2d902ff3927f
Igor Braeckevelt <igor.braeckevelt@gmail.com>
2025-09-12 15:28:30 +0200

    some changes

---
+++ igor_braeckevelt/example.py
@@ -0,0 +1,11 @@
+def example_function():
+    print("My name is Igor Braeckevelt")
+    print("My birthplace is Brasschaat, Belgium")
+
+
+def main():
+    example_function()
+
+
+if __name__ == "__main__":
+    main()
