c401ef1093cd0db7417a66769eb1b79927e91846
mlaj24 <36448042+mlaj24@users.noreply.github.com>
2025-09-12 15:41:26 +0200

    Update name_printer.py

--- adrien_ml/name_printer.py
+++ adrien_ml/name_printer.py
@@ -12,5 +12,18 @@ def print_name(name: str) -> str:
     print(f"Name: {name}")
     return name
 
+def print_birthplace(name: str, birthplace: str) -> str:
+    """
+        Prints the given name and birthplace.
+        Args:
+            name (str): The name to print.
+            birthplace (str): The birthplace to print.
+            
+        Returns:
+            str: The printed name and birthplace.
+    """
+    print(f"{name} was born in {birthplace}.")
+    return f"{name} was born in {birthplace}."
+
 if __name__ == "__main__":
-    print_name("Adrien")
+    print_birthplace("Adrien", "Paris")
