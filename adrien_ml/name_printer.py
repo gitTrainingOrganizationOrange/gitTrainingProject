a4d665a936376fbef5eccdee8ae97dc1db8af90d
mlaj24 <36448042+mlaj24@users.noreply.github.com>
2025-09-12 15:30:09 +0200

    Update name_printer.py

--- adrien_ml/name_printer.py
+++ adrien_ml/name_printer.py
@@ -1,12 +1,16 @@
 """Modules for printing names."""
 
-def print_name(name: str) -> None:
+def print_name(name: str) -> str:
     """
         Prints the given name.
         Args:
             name (str): The name to print.
+            
+        Returns:
+            str: The printed name.
     """
     print(f"Name: {name}")
+    return name
 
 if __name__ == "__main__":
     print_name("Adrien")
